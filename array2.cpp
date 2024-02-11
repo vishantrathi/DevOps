5e81410dad6cdfc7ed10e17a5c9db4d4588b5a8f
vishantrathi <vishantrathigurjar@gmail.com>
2024-02-11 17:49:56 +0530

    DAY 11

---
+++ array2.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int score[10];
+    score[0]=11;
+    score[1]=12;
+    score[2]=13;
+    score[3]=14;
+    score[4]=15;
+    score[5]=16;
+    score[6]=17;
+    score[7]=18;
+    score[8]=19;
+    score[9]=20;
+    score[10]=21;
+    cout<<"score at 0 index is  "<<score[0]<<endl;
+    cout<<"score at 1 index is  "<<score[1]<<endl;
+    cout<<"score at 2 index is  "<<score[2]<<endl;
+    cout<<"score at 3 indexs is "<<score[3]<<endl;
+    cout<<"score at 4 index is  "<<score[4]<<endl;
+    cout<<"score at 5 index is  "<<score[5]<<endl;
+    cout<<"score at 6 index is  "<<score[6]<<endl;
+    cout<<"score at 7 index is  "<<score[7]<<endl;
+    cout<<"score at 8 index is  "<<score[8]<<endl;
+    cout<<"score at 9 index is  "<<score[9]<<endl;
+    cout<<"score at 10 index is "<<score[10]<<endl;
+}
