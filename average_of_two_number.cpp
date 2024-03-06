6827138b16857297445501f3ab6cba8281ee84bd
vishantrathi <vishantrathigurjar@gmail.com>
2024-03-06 19:27:15 +0530

    Update function to handle edge cases

---
+++ average_of_two_number.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter two numbers: ";
+    cin>>a>>b;
+    cout<<"average of "<<a<< b<<"is"<< (a+b)/2;
+    return 0;
+}
