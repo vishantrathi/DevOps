b8bdf6f58f8ed388323229a9e5b8a6e5fdcc098e
vishantrathi <vishantrathigurjar@gmail.com>
2024-07-18 22:36:18 +0530

    Basics of java

---
+++ JAVA/basicofjava.java
@@ -0,0 +1,7 @@
+public class basicofjava{ //public class , public class and file name should be the same
+    public static void main(String args[]){  //main function
+
+    }
+}
+
+// above all is the boiler plate code
---
+++ JAVA/comments.java
@@ -0,0 +1,10 @@
+public class comments {
+    public static void main(String args[]){
+        // two ways to comments in java
+
+        //comments
+
+        /* comments */
+    }
+    
+}
