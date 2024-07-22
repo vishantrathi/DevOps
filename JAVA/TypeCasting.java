fc9f54606f8c194a8b3d4ef9ae250d14cd85e0af
vishantrathi <vishantrathigurjar@gmail.com>
2024-07-22 22:42:30 +0530

    type casting

--- JAVA/TypeCasting.java
+++ JAVA/TypeCasting.java
@@ -1,4 +1,12 @@
 public class TypeCasting {
-    public Static
+    public static void main(String arg[]){
+        //Typecasting in Java is the process of converting one data type to another data type using the casting operator
+        float a=25.12f;
+        int b = (int)a;
+        System.out.println(b);
+        
+
+
+    }
     
 }
