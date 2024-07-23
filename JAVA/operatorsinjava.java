64f9af77993c118579614854cadd158f7fa59274
vishantrathi <vishantrathigurjar@gmail.com>
2024-07-23 23:52:56 +0530

    commit

---
+++ JAVA/operatorsinjava.java
@@ -0,0 +1,26 @@
+public class operatorsinjava {
+    public static void main(String args[]){
+        //arithmetic operator
+        //logical operators
+        //relational operator
+        //bitwise operator
+        //Assignment operator
+
+
+        /*arithmetic operators
+         * +
+         * -
+         * /
+         * %
+         * ++ (unary operator)
+         * -- (unary operator)
+         */
+
+         /*Unary operator
+          * add or subtract the value by one
+          */
+          
+    }
+
+    
+}
