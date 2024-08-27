a6d739220079a49f193f3363745d6f15243aa1ec
vishantrathi <vishantrathigurjar@gmail.com>
2024-08-27 23:27:38 +0530

    commit

--- Striver DSA/function.cpp
+++ Striver DSA/function.cpp
@@ -1,7 +1,14 @@
 #include<iostream>
 using namespace std;
-//
+//Function are set of code which perform something for you
+//these are to modularise code
+//used to increse readibility
+//they are used same code multiple time
+//void-no return
+//parameterised
+//non-parameterised
+
 int main()
 {
-
+    
 }
